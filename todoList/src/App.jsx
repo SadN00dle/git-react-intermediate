57858053823bc8d274418de6e116bf4a3be197f5
Sad Noodle <sadnoodle11@gmail.com>
2024-07-24 14:52:41 +0545

    starting local storage get and set

--- todoList/src/App.jsx
+++ todoList/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { TodoProvider } from './contexts'
 
 function App() {
@@ -19,6 +19,26 @@ function App() {
   const deleteTodo = (id) =>{
     setTodos((prev)=> prev.filter((prevTodo) => todo.id !== id ))
   }
+
+  const toggleComplete =(id)=>{
+    setTodos((prev)=> prev.map((prevTodo)=>{
+      prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo
+    }))
+  }
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if( todos && todos.length > 0){
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(()=>{
+
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
